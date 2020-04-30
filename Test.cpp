875e61e29ef780298e085d98c862bb09a3900c2b
op1995 <pellerofir@gmail.com>
2020-04-30 17:12:24 +0300

    created tests file

---
+++ Test.cpp
@@ -0,0 +1,120 @@
+#include "doctest.h"
+#include "solver.hpp"
+using namespace solver;
+
+using namespace std;
+
+//10
+
+TEST_CASE("placeHolder") {
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+
+}
